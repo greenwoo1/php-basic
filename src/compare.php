8a3e026874b7d014ad0e15c25045c5a936bfa7e5
greenwoo <anton.mazur@jatapp.com>
2024-07-11 16:39:52 +0300

    I finished the 3rd homework

--- src/compare.php
+++ src/compare.php
@@ -9,47 +9,49 @@ $boolVar = true;
 $floatVar = 42.0;
 
 //==
-//var_dump($intVar == $stringVar);
-//
-//var_dump($intVar == $boolVar);
-//
-//var_dump($intVar == $floatVar);
-//
-//var_dump($stringVar == $boolVar);
-//
-//var_dump($floatVar == $boolVar);
-//
-////===
-//
-//var_dump($intVar === $stringVar);
-//
-//var_dump($intVar === $boolVar);
-//
-//var_dump($intVar === $floatVar);
-//
-//var_dump($stringVar === $boolVar);
-//
-//var_dump($floatVar === $boolVar);
+var_dump($intVar == $stringVar);
+
+var_dump($intVar == $boolVar);
+
+var_dump($intVar == $floatVar);
+
+var_dump($stringVar == $boolVar);
+
+var_dump($floatVar == $boolVar);
+
+//===
+
+var_dump($intVar === $stringVar);
+
+var_dump($intVar === $boolVar);
+
+var_dump($intVar === $floatVar);
+
+var_dump($stringVar === $boolVar);
+
+var_dump($floatVar === $boolVar);
 
 //==
 
-//var_dump ((int)$intVar == (string)$stringVar);
-//
-//var_dump($intVar == $boolVar);
-//
-//var_dump($intVar == $floatVar);
-//
-//var_dump($stringVar == $boolVar);
-//
-//var_dump($floatVar == $boolVar);
+var_dump ((int)$intVar == (string)$stringVar);
+
+var_dump($intVar == $boolVar);
+
+var_dump($intVar == $floatVar);
+
+var_dump($stringVar == $boolVar);
+
+var_dump($floatVar == $boolVar);
+
+//===
 
 var_dump((int)$intVar === (int)$stringVar);
 
-//var_dump($intVar === $boolVar);
-//
-//var_dump($intVar === $floatVar);
-//
-//var_dump($stringVar === $boolVar);
-//
-//var_dump($floatVar === $boolVar);
+var_dump($intVar === $boolVar);
+
+var_dump($intVar === $floatVar);
+
+var_dump($stringVar === $boolVar);
+
+var_dump($floatVar === $boolVar);
 
