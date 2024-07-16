4fc142dc038406edf286df1e52e8b0487bc73f89
greenwoo <anton.mazur@jatapp.com>
2024-07-16 15:31:07 +0300

    Finished the first task. Calculating the area of a circle

---
+++ src/circle.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+$radius = null;
+$value = null;
+
+
+echo "Enter the radius of the circle" . PHP_EOL;
+
+$value = fgets(STDIN);
+
+$radius = converter($value);
+
+echo circleSquare($radius, pi()) . PHP_EOL;
+
+function converter($string): float
+{
+    if (is_numeric($string) === false) {
+        echo "Error! The radius must be a number." . PHP_EOL;
+        exit();
+    }
+    return (float)$string;
+}
+
+function circleSquare(float $radius, float $pi): float
+{
+    return round($pi * ($radius ** 2), 2);
+}
+
+
+
+
--- src/lesson4.php
+++ src/lesson4.php
@@ -70,29 +70,29 @@ function printer($name)
 //}
 
 
-function printSum(float $a, float $b): float|false
-{
-    if (!is_numeric($a) || !is_numeric($b)) {
-        return false;
-    }
-    return $a + $b;
-};
+//function printSum(float $a, float $b): float|false
+//{
+//    if (!is_numeric($a) || !is_numeric($b)) {
+//        return false;
+//    }
+//    return $a + $b;
+//};
 
 //echo printSum(5, 5);
 
-function hello(): void
-{
-    echo "Hello world" . PHP_EOL;
-}
+//function hello(): void
+//{
+//    echo "Hello world" . PHP_EOL;
+//}
 
-function test(bool $switch): never
-{
-    if ($switch === false) {
-
-    }
-
-    exit();
-}
+//function test(bool $switch): never
+//{
+//    if ($switch === false) {
+//
+//    }
+//
+//    exit();
+//}
 
 //function apiFetch(string $uuid): mixed
 //{
@@ -126,24 +126,113 @@ function test(bool $switch): never
 //
 //search(salaryMax: '3000');
 
-function sum(...$numbers)
+//function sum(...$numbers)
+//{
+//    var_dump($numbers);
+//}
+//
+//sum(5);
+//
+//function div(float $number, int $div = 2): float
+//{
+//    if ($div < 2 ) {
+//        return $number;
+//    }
+//
+//    return $number / $div;
+//}
+
+//function dev2(float &$number, int $div = 2): void
+//{
+//    if ($div > 1 ) {
+//        $number = $number / $div;
+//    }
+//}
+
+//$mynumber = 10;
+//
+//$result = div($mynumber);
+//
+//echo $result . PHP_EOL;
+
+function discount(float &$total, float $discount = 100): void
 {
-    var_dump($numbers);
+    $total -= $discount;
 }
 
-sum(5);
+$discount = true;
+$total = 3500;
 
-function div(float $number, int $div = 2): float
+if ($discount === true) {
+    discount($total);
+}
+
+//echo $total . PHP_EOL;
+
+
+//statis, globals
+
+
+$function = function (int $a, int $b): int
 {
-    if ($div < 2 ) {
-        return $number;
+    return $a + $b;
+};
+
+//var_dump($function);
+//
+//echo $function(5, 5) . PHP_EOL;
+
+function myFilter(string $string, ?Closure $function = null): string
+{
+    if ($function !== null) {
+        return $function($string);
     }
 
-    return $number / $div;
+    return $string;
 }
 
-$mynumber = 10;
+$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis ante eu rhoncus pulvinar. Quisque 
+feugiat dapibus efficitur. Praesent imperdiet enim vel leo fermentum, nec vehicula augue ullamcorper. 
+Pellentesque fringilla sem neque, nec sodales nunc blandit congue. Ut vestibulum sapien sodales placerat dapibus. 
+Proin tellus mauris, interdum a varius nec, congue sed lorem. Suspendisse vehicula id magna vel rhoncus. 
+Suspendisse nec purus nisl. Fusce ex odio, malesuada non ligula nec, tempor dapibus arcu.";
 
-$result = div($mynumber);
 
-echo $result . PHP_EOL;
+$filterFunc = function (string $string): string {
+    return str_replace('o','*',$string);
+};
+
+//echo myFilter($text, $filterFunc) . PHP_EOL;
+
+$a = 5;
+
+function calc(float $number): float
+{
+    global $a;
+    return $a = $a * $number;
+}
+
+//echo $a . PHP_EOL;
+//
+//echo calc(5) . PHP_EOL;
+//
+//echo $a . PHP_EOL;
+
+$function = fn (float $number, int $pow = 2) => $number ** $pow;
+
+echo $function(5) . PHP_EOL;
+
+$text = myFilter('amnet', fn ($string) => str_replace(' ', '', $string));
+
+function counter(): void
+{
+    static $counter = 0;
+    $counter++;
+    echo $counter . PHP_EOL;
+}
+
+counter();
+counter();
+counter();
+counter();
+counter();
