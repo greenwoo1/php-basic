e10bf45d214a174e68f4edc05d22c85cafcf4c7b
greenwoo <anton.mazur@jatapp.com>
2024-10-14 21:39:45 +0300

    Modify homework

--- src/sort.php
+++ src/sort.php
@@ -7,20 +7,32 @@ function createArray(int $length): array
     $array = [];
 
     for ($i = 0; $i < $length; $i++) {
-        $array[] = rand(1, 100);
+        $array[] = rand(1, $length);
     }
 
     return $array;
 }
 
-function sortMyArray(array $array): void
+function sortMinMax(array $array): void
 {
     echo "Max :" . max($array) . PHP_EOL;
     echo "Min : " . min($array) . PHP_EOL;
 }
 
+function sortMyArray(array $array): array
+{
+    asort($array);
+
+    return $array;
+}
+
+
 $array = createArray(20);
 
-sortMyArray($array);
+sortMinMax($array);
+
+$array = sortMyArray($array);
+
+print_r($array);
 
 
